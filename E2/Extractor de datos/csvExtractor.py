0e542072c8807aa886319d9590032b27b5ed9045
TheMightyTsar <fernandeznicolas0505@gmail.com>
2023-05-21 21:06:36 -0400

    data extractor

--- E2/Extractor de datos/csvExtractor.py
+++ E2/Extractor de datos/csvExtractor.py
@@ -6,9 +6,22 @@ def abrir_csv(archivo):
     return lineas
 
 
-def extraer_datos():
-    
+def escribir_csv(archivo, lineas):
+    carpeta = 'PoblarTablas/'
 
+    with open(f'{carpeta}{archivo}.csv', "w", encoding="utf-8") as archivo_csv:
+        # Escribir los datos en el archivo
+        for fila in lineas:
+            linea = ','.join(str(valor) for valor in fila)  # Convertir los valores de la fila en una línea de texto separada por comas
+            archivo_csv.write(linea + '\n')  # Escribir la línea en el archivo
+
+
+    return lineas
+
+
+def extraer_datos():
+    # Productos
+    lineas_productos = abrir_csv('productos')
 
     # se trabaja la lista
     lista_id = []
@@ -18,6 +31,3 @@ def extraer_datos():
     for elem in lista_txt:
         linea = elem.split(",")
     return lista_listas
-
-
-
