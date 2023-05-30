ddf7d9dfdceeefc8c2752f379e22f81663e07930
Nicolás Fernández <fernandeznicolas0505@gmail.com>
2023-05-30 11:47:51 -0400

    working in views and login

--- E3/paginaWEB/index.php
+++ E3/paginaWEB/index.php
@@ -1,1 +1,19 @@
-<?php
+<?php include('templates/header.html');   ?>
+<body>
+<h1> Sistemas de: tienda 47-54</h1>
+<br>
+<br>
+<p style="text-align:center;">Ingrese sesion para poder continuar </p>
+<br>
+<form action="view_handler.php" method="POST">
+    <label for="username">Nombre de usuario:</label>
+    <input type="text" id="username" name="username" required>
+
+    <label for="password">Contraseña:</label>
+    <input type="password" id="password" name="password" required>
+
+    <input type="submit" value="Enviar">
+</form>
+
+
+</body>
